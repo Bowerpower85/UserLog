b82364c3eaa5171031c02b8e945827f2b12fbd50
Jonathan Bower <bowerpower85@gmail.com>
2020-12-11 11:42:22 -0800

    Added an array of my group info in group.json

--- userlog/src/App.js
+++ userlog/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import CardWrap from "./components/CardWrap";
 import Header from "./components/Header";
+import groupArray from "./group.json";
 
 function App() {
   return (
